7141681610adc535be774e0a84d4608becb6c1f6
David William <46327096+sluucke@users.noreply.github.com>
2021-10-28 18:49:57 -0300

    ⚙ - Some fixes in API

--- utils/db.js
+++ utils/db.js
@@ -3,36 +3,35 @@ import mongoose from 'mongoose'
 const connection = {}
 
 async function connect() {
-  if(connection.isConnected) {
-    console.log('Already connected')
-    return
-  }
-  if(mongoose.connections.length > 0) {
-    connection.isConnected = mongoose.connections[0].readyState
-    if(connection.isConnected === 1) {
-      console.log('Use previos connection')
-      return
+    if(connection.isConnected) {
+        console.log('already connected')
+        return;
     }
-    await mongoose.disconnect()
-  }
-  const db = await mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-
-  console.log('New connection')
-  connection.isConnected = db.connection[0].readyState
+    if(mongoose.connections.length > 0) {
+        connection.isConnected = mongoose.connections[0].readyState
+        if(connection.isConnected === 1) {
+            console.log('use previous connection')
+            return;
+        } 
+        await mongoose.disconnect()
+    }
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    console.log('new connection')
+    connection.isConnected = db.connections[0].readyState
 }
 
 async function disconnect() {
-  if(connection.isConnected) {
-    if(process.env.NODE_ENV === 'production') {
-      await mongoose.disconnect()
-      connection.isConnected = false
-    } else {
-      console.log('Not disconnected')
+    if(connection.isConnected) {
+        if(process.env.NODE_ENV === 'production') {
+            await mongoose.disconnect()
+            connection.isConnected = false
+        } else {
+            console.log('not disconnected')
+        }
     }
-  }
 }
 
 
