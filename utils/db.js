312b16548c96c4f2d04ddc02be7a1f3ff72690de
sluucke <sluuckejoohn@gmail.com>
2021-10-28 21:57:48 +0000

    ⚙ - Some fixes in API

--- utils/db.js
+++ utils/db.js
@@ -7,14 +7,6 @@ async function connect() {
         console.log('already connected')
         return;
     }
-    if(mongoose.connections.length > 0) {
-        connection.isConnected = mongoose.connections[0].readyState
-        if(connection.isConnected === 1) {
-            console.log('use previous connection')
-            return;
-        } 
-        await mongoose.disconnect()
-    }
     const db = await mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
